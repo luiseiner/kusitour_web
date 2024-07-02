825be99d09fb1a4f119f45aee27783bbe060a9e1
Luiseiner <luis.islachin@tecsup.edu.pe>
2024-07-02 02:47:41 -0500

    first commit

--- kusitour/settings.py
+++ kusitour/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 INSTALLED_APPS = [
